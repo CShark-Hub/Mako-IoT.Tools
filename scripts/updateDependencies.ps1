38c094aa0a032a631ae7bf037b976afacb0a42a3
Marcin Torba <torbamarcin@gmail.com>
2023-01-28 01:03:52 +0100

    Auto update (#5)
    * Update 1 NuGet dependencies
    
    Bumps Nerdbank.GitVersioning from 3.5.113 to 3.5.119</br>
    [version update]
    
    ### :warning: This is an automated update. :warning:
    
    * Update
    
    * Fix script
    
    * Fixes
    
    * Fix
    
    * fix
    
    * Fix
    
    * fix
    
    * fix
    
    * Fix
    
    * update
    
    * fix
    
    * fix
    
    * upd
    
    * fix
    
    * Update azue-pipelines-update-nugets.yml for Azure Pipelines
    
    * Update azue-pipelines-update-nugets.yml for Azure Pipelines
    
    * Update azue-pipelines-update-nugets.yml for Azure Pipelines
    
    * Update updateDependencies.ps1
    
    * Update updateDependencies.ps1
    
    * Update updateDependencies.ps1
    
    * Update updateDependencies.ps1
    
    * Update azue-pipelines-update-nugets.yml for Azure Pipelines
    
    * Update azue-pipelines-update-nugets.yml for Azure Pipelines
    
    * Update azue-pipelines-update-nugets.yml for Azure Pipelines
    
    * Update azue-pipelines-update-nugets.yml for Azure Pipelines
    
    * Update azue-pipelines-update-nugets.yml for Azure Pipelines
    
    * Update azue-pipelines-update-nugets.yml for Azure Pipelines
    
    * Update azue-pipelines-update-nugets.yml for Azure Pipelines
    
    * Update azue-pipelines-update-nugets.yml for Azure Pipelines
    
    * Update script.
    
    * update script
    
    * Update azue-pipelines-update-nugets.yml for Azure Pipelines
    
    * udpate script
    
    * update script
    
    * update script
    
    * update pipeline
    
    * update pipeline
    
    * Add remove branch
    
    * Add Delete branch
    
    * Revert "Add Delete branch"
    
    This reverts commit 35ee169e68a129892b7eb71148241f31a46244be.
    
    * Revert "Add remove branch"
    
    This reverts commit cb4a1cb4876770b08e6c93516b11a68bb2f12d02.
    
    * Case with removebranch (#4)
    
    * Delete folders
    
    * Remove variable
    
    * Update fix
    
    * Disabling a single repo
    
    Co-authored-by: blizni79 <nanoframework@outlook.com>
    
    * Update script.
    
    * Update script.
    
    * Remove folders.
    
    ---------
    
    Co-authored-by: nfbot <nanoframework@outlook.com>
    Co-authored-by: zulos999 <81568731+zulos999@users.noreply.github.com>
    Co-authored-by: Blizni79 <77400143+blizni79@users.noreply.github.com>

---
+++ scripts/updateDependencies.ps1
@@ -0,0 +1,147 @@
+param(
+    [Parameter(Mandatory = $true)][String]$organization,
+    [Parameter(Mandatory = $true)][String]$gitHubToken,
+    [Parameter(Mandatory = $true)][String]$gitHubUser)
+
+$ErrorActionPreference = "Stop"
+$SleepTimeInSecondsToRunChecks = 10
+$maxRetryCount = 10
+$sleepBetweenRetries = 60;
+
+Function Test-CommandExists {
+    Param ($command)
+    $oldPreference = $ErrorActionPreference
+    $ErrorActionPreference = "Stop"
+    try {
+        if (Get-Command $command) {
+            return $true
+        }
+    }
+    Catch {
+        return $false
+    }
+    Finally {
+        $ErrorActionPreference = $oldPreference
+    }
+}
+
+function IsPrCreatedByNanodu {
+    Param ($login, $title)
+
+    if ($login -eq $gitHubUser -And $prs.title -match 'Update (\d*) NuGet dependencies') {
+        return $true
+    }
+
+    return $false
+}
+
+function SetPrMergeSqush {
+    param ($prNumber)
+    Write-Host "Auto merging"$prNumber
+    $url = "https://api.github.com/repos/$organization/$repoName/pulls/$prNumber/merge"
+    $data = @{        
+        merge_method = "squash"
+    };
+    $json = $data | ConvertTo-Json;
+    Invoke-RestMethod -Method PUT -Uri $url -ContentType "application/json" -Headers @{"Authorization" = $tokenHeader } -Body $json;
+}
+
+function DeleteBranch {
+    param ($prBranchName)
+    Write-Host 'Remove Branch'$prBranchName ;
+    $url = "https://api.github.com/repos/$organization/$repoName/git/refs/heads/$prBranchName"
+    Invoke-RestMethod -Method DELETE -Uri $url -ContentType "application/json" -Headers @{"Authorization" = $tokenHeader };
+}
+
+function CanSafeDeleteBranch {
+    param ($prNumber)
+    param ($retry = 0)
+
+    Write-Host 'Check if branch is safe to delete. Retry'$prBranchName ;
+    $url = "https://api.github.com/repos/$organization/$repoName/pulls/$prNumber"
+    $response = Invoke-WebRequest -Uri $url -Headers @{"Authorization" = $tokenHeader }
+    $pr = $response | ConvertFrom-Json
+
+    if ($pr.state -eq 'closed'){
+        return
+    }
+
+    if ($retry -eq $maxRetryCount){
+        Write-Warning 'Unable to delete branch from PR '$prNumber
+        return
+    }
+
+    Write-Host 'Waiting for' $sleepBetweenRetries 'before checking if delete is safe.'
+    Start-Sleep $sleepBetweenRetries
+    $newRetry = $retry + 1;
+    CanSafeDeleteBranch $prNumber $newRetry 
+}
+
+Write-Host "Running script with parameters: "
+Write-Host "Organization: $organization"
+Write-Host "GitHub user: $gitHubUser"
+Write-Host "Github token lenght: "$gitHubToken.Length
+Write-Debug "Github token: $gitHubToken"
+Write-Host ""
+Write-Host ""
+
+$env:GIT_REDIRECT_STDERR = '2>&1'
+$env:GITHUB_TOKEN = $gitHubToken
+
+If (!(Test-CommandExists "nanodu")) {
+    Write-Host "nanodu command not exists. Trying to install."
+    dotnet nuget add source https://pkgs.dev.azure.com/nanoframework/feed/_packaging/sandbox/nuget/v3/index.json --name nano
+    dotnet tool install nanodu -g
+    Write-Host "nanodu installed."
+    dotnet nuget remove source nano
+}
+$request = "https://api.github.com/orgs/$organization/repos?per_page=100"
+Write-Host "Executing request "$request
+$tokenHeader = "Bearer $gitHubToken"
+$response = Invoke-WebRequest -Uri $request -Headers @{"Authorization" = $tokenHeader }
+$repositories = $response | ConvertFrom-Json
+
+foreach ($repository in $repositories) {
+    if ($repository.full_name -notlike "*Mako-IoT.Device*") {
+        continue;
+    }
+
+    $env:NF_Library = $repository.name
+
+    Write-Host "Trying to update"$repository.full_name
+
+    if (Test-Path -Path $repository.name) {
+        Remove-Item -Recurse -Force $repository.name
+    }
+
+    $Expr = 'nanodu --git-hub-user $gitHubUser --use-git-token-for-clone true --repo-owner $organization --repos-to-update $repository.name'
+    Invoke-Expression $Expr
+}
+
+Write-Host "Sleeping for" $SleepTimeInSecondsToRunChecks " to allow GitHub to initialize checks"
+Start-Sleep $SleepTimeInSecondsToRunChecks
+
+# Close any PR if created by nanodu
+foreach ($repository in $repositories) {
+    $repoName = $repository.name
+    $requestPr = "https://api.github.com/repos/$organization/$repoName/pulls"
+    Write-Host "Executing request "$requestPr
+    $tokenHeader = "Bearer $gitHubToken"
+    $response = Invoke-WebRequest -Uri $requestPr -Headers @{"Authorization" = $tokenHeader }
+    $prs = $response | ConvertFrom-Json
+
+    foreach ($pr in $prs) {
+        if (!(IsPrCreatedByNanodu $prs.user.login $prs.title)) {
+            continue
+        }
+    
+        SetPrMergeSqush $pr.number  
+        
+        if (CanSafeDeleteBranch $pr.number) {
+            DeleteBranch $prs.head.ref
+        }
+    }
+}
+
+Write-Host "Done"
+exit 0
